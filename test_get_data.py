9a739da696c8f0ec4048687190016fb2392ca374
owenmadin <owen.madin@colorado.edu>
2019-09-26 22:21:51 -0600

    Created input type test, failing

--- test_get_data.py
+++ test_get_data.py
@@ -5,4 +5,9 @@ class TestGetData(unittest.TestCase):
     def test_read_stdin_col_no_input(self):
         # Test if there is input passed to the function
         self.assertRaises(TypeError,gd.read_stdin_col,None)
-        
+    def test_read_stdin_col_type(self):
+        # test to see if the correct type is passed to the function
+        self.assertRaises(TypeError,gd.read_stdin_col,'text')
+        self.assertRaises(TypeError,gd.read_stdin_col,1.23)
+        self.assertRaises(TypeError,gd.read_stdin_col,[1,2,3])
+        self.assertRaises(TypeError,gd.read_stdin_col,{})        
