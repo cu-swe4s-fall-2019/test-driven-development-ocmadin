6f0b4f84fc5b60fae5523e24b3a9c67969e7e8a3
owenmadin <owen.madin@colorado.edu>
2019-09-26 20:44:11 -0600

    No input test passing

--- math_lib.py
+++ math_lib.py
@@ -1,5 +1,21 @@
 
 def list_mean(L):
+    """
+    Function to compute the mean of a list
+    
+    Arguments
+    ---------
+    L : list of floats/ints
+        List to compute the mean of
+    
+    Returns
+    -------
+    mean : float
+        The mean of the list
+    """
+    if L is None:
+        raise TypeError("list_mean: No input passed to function")
+    
     return None
 
 def list_stdev(L):
