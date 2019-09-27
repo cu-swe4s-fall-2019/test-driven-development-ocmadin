f303c2d6828533c410f4c5468afb80cfbdceaa7b
owenmadin <owen.madin@colorado.edu>
2019-09-26 23:18:28 -0600

    Bad input type in list test passing

--- data_viz.py
+++ data_viz.py
@@ -4,6 +4,11 @@ def boxplot(L, out_file_name='boxplot.png'):
         raise TypeError('boxplot: No input to L')
     if not isinstance(L,list):
         raise TypeError('boxplot: must pass a list to L')
+        
+    valid_types = [isinstance(value,(int,float,complex)) for value in L]
+    
+    if not any(valid_types):
+        raise TypeError("boxplot: Invalid types in list")
     pass
 
 def histogram(L, out_file_name):
