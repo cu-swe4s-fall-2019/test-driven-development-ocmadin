a67f56779864b50413c4f7946e41b699d66b5996
owenmadin <owen.madin@colorado.edu>
2019-09-26 23:15:12 -0600

    Wrong input type test passing

--- data_viz.py
+++ data_viz.py
@@ -1,7 +1,9 @@
 
-def boxplot(L, out_file_name='figure.png'):
+def boxplot(L, out_file_name='boxplot.png'):
     if L is None:
-        raise TypeError('boxplot: Must pass a list to L')
+        raise TypeError('boxplot: No input to L')
+    if not isinstance(L,list):
+        raise TypeError('boxplot: must pass a list to L')
     pass
 
 def histogram(L, out_file_name):
