58362f2774bb0bcc7ee848782e2771cc319bf281
owenmadin <owen.madin@colorado.edu>
2019-09-26 20:50:24 -0600

    List input type failing

--- test_math_lib.py
+++ test_math_lib.py
@@ -6,3 +6,11 @@ class TestListMean(unittest.TestCase):
     def test_list_mean_no_input(self):
         # test if no input is passed to the function
         self.assertRaises(TypeError, ml.list_mean, None)
+    
+    def test_list_mean_type(self):
+        # test if the input is the wrong type
+        self.assertRaises(TypeError, ml.list_mean, 1)
+        self.assertRaises(TypeError, ml.list_mean,1.05)
+        self.assertRaises(TypeError, ml.list_mean,{})
+        self.assertRaises(TypeError, ml.list_mean,True)
+        self.assertRaises(TypeError, ml.list_mean,'Text')
