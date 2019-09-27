55c0c41ceadc61b720fd8cad1a36193b75844110
owenmadin <owen.madin@colorado.edu>
2019-09-26 23:13:07 -0600

    Created wrong input type test, failing

--- test_data_viz.py
+++ test_data_viz.py
@@ -4,3 +4,7 @@ import data_viz as dv
 class TestBoxplot(unittest.TestCase):
     def test_boxplot_empty_input(self):
         self.assertRaises(TypeError, dv.boxplot,None)
+    def test_boxplot_wrong_input_type(self):
+        self.assertRaises(TypeError,dv.boxplot,1)
+        self.assertRaises(TypeError,dv.boxplot,1.5)
+        self.assertRaises(TypeError,dv.boxplot,'text')
