c940ff29556c728065c4b487787506903b88c5bc
owenmadin <owen.madin@colorado.edu>
2019-09-26 23:42:08 -0600

    Type error tests passing

--- data_viz.py
+++ data_viz.py
@@ -26,8 +26,22 @@ def boxplot(L, out_file_name='boxplot.png'):
     try:
         plt.savefig(out_file_name, bbox_inches='tight')
     except ValueError:
+        raise ValueError
+
 
 def histogram(L, out_file_name):
+    if L is None:
+        raise TypeError('boxplot: No input to L')
+    if not isinstance(L,list):
+        raise TypeError('boxplot: must pass a list to L')
+        
+    valid_types = [isinstance(value,(int,float,complex)) for value in L]
+    
+    if not any(valid_types):
+        raise TypeError("boxplot: Invalid types in list")
+        
+    if not isinstance(out_file_name,str):
+        raise TypeError("boxplot: File name must be str")
     pass
 
 def combo(L, out_file_name):
