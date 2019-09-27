78e315d1e98c055b30986f78db78bb5d08f1f704
owenmadin <owen.madin@colorado.edu>
2019-09-26 23:41:25 -0600

    Added type error tests

--- test_data_viz.py
+++ test_data_viz.py
@@ -20,3 +20,16 @@ class TestBoxplot(unittest.TestCase):
 class TetHistogram(unittest.TestCase):
     def test_histogram_empty_input(self):
         self.assertRaises(TypeError, dv.histogram,None)
+    def test_histogram_wrong_input_type(self):
+        self.assertRaises(TypeError,dv.histogram,1)
+        self.assertRaises(TypeError,dv.histogram,1.5)
+        self.assertRaises(TypeError,dv.histogram,'text')
+    def test_histogram_bad_input_in_list(self):
+        self.assertRaises(TypeError, dv.histogram,['text','text'])
+        self.assertRaises(TypeError, dv.histogram,[[1,2],[1,2]])
+    def test_histogram_filename_type(self):
+        self.assertRaises(TypeError, dv.histogram,[1,2,3],123)
+        self.assertRaises(TypeError, dv.histogram,[1,2,3],['text','text','text'])
+    def test_histogram_output_file_type(self):
+        self.assertRaises(ValueError,dv.histogram,[1,2,3],'output.txt')
+        
